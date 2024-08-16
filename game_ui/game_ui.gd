cd57cdf129a98b14875b4fc615654e254a61ea73
Jarjaroz <145113688+Jarjaroz@users.noreply.github.com>
2024-08-16 12:06:48 +0200

    staminabar en highscore

--- game_ui/game_ui.gd
+++ game_ui/game_ui.gd
@@ -1,14 +1,11 @@
 extends Control
 
 @onready var timer = $Timer
-@onready var height_label = $MarginContainer/heightLabel
+@onready var height_label = $MarginContainer/HealthBar/VBoxContainer/heightLabel
 @onready var health_bar = $MarginContainer/HealthBar
-@onready var loss_bar = $MarginContainer/HealthBar/LossBar
-@onready var loss_timer = $MarginContainer/HealthBar/LossTimer
 
-var sb = StyleBoxFlat.new()
-const SB_YELLOW_TRANS = preload("res://game_ui/sb_yellow_trans.tres")
-const SB_ORANGE_TRANS = preload("res://game_ui/sb_orange_trans.tres")
+const STAMINABAR_FILL_YEL = preload("res://assets/sprites/staminabar_fill.png")
+const STAMINABAR_FILL_RED = preload("res://assets/sprites/staminabar_fill_red.png")
 
 const STAMINA_FLAPPING_COST: int = 3
 const MAX_STAMINA: int = 500
@@ -23,10 +20,8 @@ var is_flapping: bool = false
 func _ready():
 	GameManager.max_stamina = MAX_STAMINA
 	health_bar.max_value = MAX_STAMINA
-	loss_bar.max_value = MAX_STAMINA
 	stamina = MAX_STAMINA
 	health_bar.value = stamina
-	loss_bar.value = stamina
 	
 	SignalManager.on_flower_hit.connect(on_flower_hit)
 	SignalManager.on_flapping_wing.connect(on_flapping_wing)
@@ -34,11 +29,6 @@ func _ready():
 	
 	height_label.text = str(record_height) + " cm"
 	
-	health_bar.add_theme_stylebox_override("fill", sb)
-	sb.corner_radius_bottom_left = 7
-	sb.corner_radius_bottom_right = 7
-	sb.corner_radius_top_left = 7
-	sb.corner_radius_top_right = 7
 	change_color_tint()
 
 func _on_timer_timeout():
@@ -59,22 +49,14 @@ func _on_timer_timeout():
 	height_label.text = str(record_height) + " cm"
 	GameManager.set_score(record_height)
 	stamina = clamp(stamina,0, MAX_STAMINA)
-	health_bar.value = stamina
-	if(!is_flapping):
-		if loss_bar.value > health_bar.value:
-			loss_bar.value -= STAMINA_DRAIN*6
-		else:
-			loss_bar.value = health_bar.value-1
-	else:
-		loss_bar.value -= STAMINA_DRAIN
+	health_bar.value = stamina	
 	change_color_tint()
 
 func on_flower_hit():
 	stamina += GameManager.calculate_flower_boost()
-	loss_bar.value = stamina
+
 
 func on_flapping_wing():
-	loss_timer.start()
 	is_flapping = true
 	if GameManager.started == false:
 		return
@@ -86,9 +68,9 @@ func on_revive():
 
 func change_color_tint():
 	if (health_bar.value / health_bar.max_value) > 0.35:
-		sb.bg_color = SB_YELLOW_TRANS.bg_color
+		health_bar.texture_progress = STAMINABAR_FILL_YEL
 	else:
-		sb.bg_color = SB_ORANGE_TRANS.bg_color
+		health_bar.texture_progress = STAMINABAR_FILL_RED
 
 func _on_loss_timer_timeout():
 	is_flapping = false
--- high_score/high_score.gd
+++ high_score/high_score.gd
@@ -1,5 +1,6 @@
 extends Area2D
-
+@onready var animated_sprite_2d = $AnimatedSprite2D
+var teller: int = 0
 func _ready():
 	if GameManager.highscore > 1:
 		position.y = (GameManager.highscore*-1)*100 +500
@@ -11,3 +12,8 @@ func _ready():
 func _on_body_entered(body):
 	if body.is_in_group("player"):
 		queue_free()
+
+func _process(delta):
+	teller += 1
+	position.y += 20*sin(0.1*teller)*delta
+	
